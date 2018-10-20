af461086671ab298439e3094198b3d5c29c2a26d
Adi <adi.karic@gmail.com>
2018-10-21 00:37:03 +0200

    First Commit

---
+++ src/robots.js
@@ -0,0 +1,136 @@
+export const robots = [
+	{
+		id:1,
+		name:'Isak Samokovlija',
+		username:'Isko',
+		email:'isak@gmail.com'
+	},
+	{
+		id:2,
+		name:'Zlatko Krilić',
+		username:'Zlatan',
+		email:'cudesni.zlatko@biz.net'
+	},
+	{
+		id:3,
+		name:'Ahmet Hromadžić',
+		username:'Vuk',
+		email:'okamenjen.vuk@bajke.ba'
+	},
+	{
+		id:4,
+		name:'Ivo Andrić',
+		username:'Ivica',
+		email:'ivan.ivanic@nobel.se'
+	},
+	{
+		id:5,
+		name:'Milan Oklopdžić',
+		username:'Mika',
+		email:'ca.blues@california.yu'
+	},
+	{
+		id:6,
+		name:'Borislav Pekić',
+		username:'Zombie',
+		email:'london.heathrow@besnilo.uk'
+	},
+	{
+		id:7,
+		name:'Semezdin Mehmedinović',
+		username:'Fantom',
+		email:'semko@sarajevo.ba'
+	},
+	{
+		id:8,
+		name:'Marko Vešović',
+		username:'Markec',
+		email:'marko.veš@spisatelj.com'
+	},
+	{
+		id:9,
+		name:'Selvedin Avdić',
+		username:'Podstanar',
+		email:'podstanari@gmail.com'
+	},
+	{
+		id:10,
+		name:'Meša Selimović',
+		username:'Tvrdi',
+		email:'derviš@gmail.com'
+	}
+];
+
+/*
+
+hostano ovdje --> https://api.myjson.com/bins/gthpk
+
+[
+  {
+    "id": 1,
+    "name": "Isak Samokovlija",
+    "username": "Isko",
+    "email": "isak@gmail.com"
+  },
+  {
+    "id": 2,
+    "name": "Zlatko Krilić",
+    "username": "Zlatan",
+    "email": "cudesni.zlatko@biz.net"
+  },
+  {
+    "id": 3,
+    "name": "Ahmet Hromadžić",
+    "username": "Vuk",
+    "email": "okamenjen.vuk@bajke.ba"
+  },
+  {
+    "id": 4,
+    "name": "Ivo Andrić",
+    "username": "Ivica",
+    "email": "ivan.ivanic@nobel.se"
+  },
+  {
+    "id": 5,
+    "name": "Milan Oklopdžić",
+    "username": "Mika",
+    "email": "ca.blues@california.yu"
+  },
+  {
+    "id": 6,
+    "name": "Borislav Pekić",
+    "username": "Zombie",
+    "email": "london.heathrow@besnilo.uk"
+  },
+  {
+    "id": 7,
+    "name": "Semezdin Mehmedinović",
+    "username": "Fantom",
+    "email": "semko@sarajevo.ba"
+  },
+  {
+    "id": 8,
+    "name": "Marko Vešović",
+    "username": "Markec",
+    "email": "marko.veš@spisatelj.com"
+  },
+  {
+    "id": 9,
+    "name": "Selvedin Avdić",
+    "username": "Podstanar",
+    "email": "podstanari@gmail.com"
+  },
+  {
+    "id": 10,
+    "name": "Meša Selimović",
+    "username": "Tvrdi",
+    "email": "derviš@gmail.com"
+  }
+]
+
+
+
+
+
+
+*/
